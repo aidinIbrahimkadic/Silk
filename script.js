76522747e43fb91a904d657e0d9ea482466c17ef
aidinIbrahimkadic <aidin.ibrahimkadic94@gmail.com>
2025-02-16 22:31:19 +0100

    kartica

--- script.js
+++ script.js
@@ -31,3 +31,24 @@ function toggleCart() {
 function removeProduct(element) {
   element.parentElement.remove();
 }
+
+document.querySelectorAll(".kartica").forEach((kartica) => {
+  const step = parseInt(kartica.querySelector(".step-value").textContent); // Get step value
+  const input = kartica.querySelector("#counter");
+  const decreaseBtn = kartica.querySelector(".btn:first-child");
+  const increaseBtn = kartica.querySelector(".btn:last-child");
+
+  // Increase value by step
+  increaseBtn.addEventListener("click", () => {
+    input.value = parseInt(input.value) + step;
+  });
+
+  // Decrease value by step, but allow reaching 0
+  decreaseBtn.addEventListener("click", () => {
+    let currentValue = parseInt(input.value);
+    if (currentValue > 0) {
+      // Allows reaching 0 but not negative values
+      input.value = currentValue - step;
+    }
+  });
+});
