e13de4ebae9734dbc59ba924929b215c9b88a9fc
aidinIbrahimkadic <aidin.ibrahimkadic94@gmail.com>
2025-02-16 18:31:17 +0100

    sidebar zavrsen

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 const input = document.getElementById("myInput");
 const clearBtn = document.querySelector(".clear-btn");
 
-input.addEventListener("input", function() {
-    if (this.value.length > 0) {
-        clearBtn.style.display = "block"; // Show 'X' when there is input
-    } else {
-        clearBtn.style.display = "none"; // Hide 'X' when empty
-    }
+input.addEventListener("input", function () {
+  if (this.value.length > 0) {
+    clearBtn.style.display = "block"; // Show 'X' when there is input
+  } else {
+    clearBtn.style.display = "none"; // Hide 'X' when empty
+  }
 });
 
 function clearInput() {
-    input.value = "";
-    clearBtn.style.display = "none"; // Hide 'X' when cleared
-    input.focus(); // Keep focus on input after clearing
+  input.value = "";
+  clearBtn.style.display = "none"; // Hide 'X' when cleared
+  input.focus(); // Keep focus on input after clearing
+}
+
+// Toggle sidebar
+function toggleCart() {
+  const cartSidebar = document.getElementById("cart-sidebar");
+  const cartOverlay = document.querySelector(".cart-overlay");
+
+  if (cartSidebar.classList.contains("open")) {
+    cartSidebar.classList.remove("open");
+    cartOverlay.classList.remove("active");
+  } else {
+    cartSidebar.classList.add("open");
+    cartOverlay.classList.add("active");
+  }
+}
+function removeProduct(element) {
+  element.parentElement.remove();
 }
